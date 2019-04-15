0b0d37a6f6734998cb5b695883dc300926c899f4
jay.zhu <jay.zhu@dadaabc.com>
2019-04-15 16:00:55 +0800

    根据环境加载配置文件

--- src/main/java/com/imooc/jay/JayApplication.java
+++ src/main/java/com/imooc/jay/JayApplication.java
@@ -2,6 +2,8 @@ package com.imooc.jay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.StandardEnvironment;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -10,5 +12,18 @@ public class JayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JayApplication.class, args);
+
+
+//        // 根据不同的环境加载不同的配置文件
+//        SpringApplication application = new SpringApplication(JayApplication.class);
+//
+//        ConfigurableEnvironment environment = new StandardEnvironment();
+//        // 设置环境，可以根据不同的环境变量，读取不同的配置文件
+//        // 例如环境为dev，加载的配置文件为application.properties和application-dev.properties，如果有重复的属性，以application-dev.properties的属性为主
+//        environment.setActiveProfiles("dev");
+//        application.setEnvironment(environment);
+//
+//
+//        application.run(args);
     }
 }
