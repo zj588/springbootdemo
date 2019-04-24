17912c72357fbf20224655cd103c686e17a50efe
jay.zhu <jay.zhu@dadaabc.com>
2019-04-24 11:15:56 +0800

    commit

--- src/main/java/com/imooc/jay/JayApplication.java
+++ src/main/java/com/imooc/jay/JayApplication.java
@@ -2,12 +2,14 @@ package com.imooc.jay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.StandardEnvironment;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+//@EnableEurekaClient
 public class JayApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/imooc/jay/config/dao/DataSourceConfiguration.java
+++ src/main/java/com/imooc/jay/config/dao/DataSourceConfiguration.java
@@ -1,36 +1,23 @@
 package com.imooc.jay.config.dao;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import com.zaxxer.hikari.HikariDataSource;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.beans.PropertyVetoException;
+import javax.sql.DataSource;
 
 @Configuration
 // 配置mybatis mapper的扫描路径
 @MapperScan("com.imooc.jay.dao")
 public class DataSourceConfiguration {
-    @Value("${jdbc.driver}")
-    private String jdbcDriver;
-    @Value("${jdbc.url}")
-    private String jdbcUrl;
-    @Value("${jdbc.username}")
-    private String jdbcUsername;
-    @Value("${jdbc.password}")
-    private String jdbcPassword;
 
     @Bean(name = "dataSource")
-    public ComboPooledDataSource createDataSource() throws PropertyVetoException {
-        ComboPooledDataSource dataSource = new ComboPooledDataSource();
-        dataSource.setDriverClass(jdbcDriver);
-        dataSource.setJdbcUrl(jdbcUrl);
-        dataSource.setUser(jdbcUsername);
-        dataSource.setPassword(jdbcPassword);
-        //关闭连接后不自动commit
-        dataSource.setAutoCommitOnClose(false);
-
-        return dataSource;
+    @ConfigurationProperties(prefix = "springstudy.datasource")
+    public DataSource createDataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
     }
 }
